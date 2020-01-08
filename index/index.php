5a93c99961a60b2da968100cfdb0606662f55342
valentin-49 <55395531+valentin-49@users.noreply.github.com>
2020-01-08 15:48:13 +0100

    final

--- index/index.php
+++ index/index.php
@@ -6,12 +6,12 @@
     </head>
         <body>
             <div id="connexion">
-            <div id="CoUsers">
+            <div id="CoUsers" align="center">
                 <form action="index.php" method="POST"> 
-                <p><label>Nom De Connexion</label>
+                <p><label><h3>Nom De Connexion</h3></label>
                 <input type="text" name="NDC_1"/></p>
                 <p> </p> 
-                <p><label>Mot De Passe</label>
+                <p><label><h3>Mot De Passe</h3></label>
                 <input type="password" name="MDP_1"/></p>
                 <p> </p>
                 <input type="submit" name="valider" value="cliquer sur valider"/>
@@ -24,45 +24,20 @@
                     $isconnectUS = $user->Compar($_POST['NDC_1'],$_POST['MDP_1']);
                     if($isconnectUS){
 
-                    echo"vous etes connectez";  
+                    echo"vous etes connectez."; 
+                    
+                    ?><input type="button" name="lien1" value="redirection" onclick="self.location.href='http://192.168.64.116/Valentin/appli_php/page_recherche/page_recherche.php'" style="background-color:#3cb371" style="color:white; font-weight:bold"onclick><?php
+                    
                     }else{
 
                         $user->UsersNv($_POST['NDC_1'] , $_POST['MDP_1']);
-                        echo"nouvelle utilisateur créer";
+                        echo"nouvelle utilisateur creer veuillez renter a nouveau vos identifiants.";
                         
                     }
                 }
 ?>
 
-            </div>
-            <div id="image_1" >
-                
-            </div>
-            <div id="CoAdmin">
-                <form action="index.php" method="POST"> 
-                <p><label>Nom De Connexion</label>
-                <input type="text" name="NDC_2"/></p>
-                <p> </p> 
-                <p><label>Mot De Passe</label>
-                <input type="password" name="MDP_2"/></p>
-                <p> </p>
-                <input type="submit" name="valider" value="cliquer sur valider"/>
-                </form>
-<?php
-                if(isset($_POST['NDC_2']) && isset($_POST['MDP_2'])){
-                    
-                    $user = new admin();
-                    $user->adminCo($_POST['NDC_2'] , $_POST['MDP_2']);
-                    $isconnectAD = $user->Compar($_POST['NDC_2'],$_POST['MDP_2']);
-                    if($isconnectAD){
-
-                    echo"vous etes connectez";  
-                    }else{
-
-                        echo"Mot de passe ou Nom incorrect, veuillez reessayer.";
-                    }
-                }
-?>
+          
             </div>
             </div>
         </body>
