ebb30f17ead33d2db6d0a6971baa52b5b5423c83
tbonny <tbonny@la-providence.net>
2020-01-09 16:48:27 +0100

    updtate

--- page_admin/page_admin.php
+++ page_admin/page_admin.php
@@ -1,11 +1,43 @@
 <?php require("gestion.php");?>
+<?php require("updtate.php");?>
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="admin.css">
     </head>
         <body>
             <div id="update">
+            <div class="titre">
+                    <h1>UPDATE</h1>
+                </div>
+                <form action="page_admin.php" method="POST">
+                        <p>modifier un véhicule :</p>
+                         <p>nom :
+                         <input type="text" name="nom"></p>
+                         <p>marque :
+                         <input type="text" name="marque"></p>
+                         <p>date de construction :
+                         <input type="text" name="date_constru"></p>
+                         <p>pays d'origine :
+                         <input type="text" name="origine"></p>
+                         <p>type de moteur :
+                         <input type="text" name="moteur"></p>
+                         <p>prix :
+                         <input type="text" name="prix"></p>
+
+                         <p>liens image :
+                         <input type="text" name="image"></p>
+
+                         <input class="bouton" type="submit" name="ajouter" value="ajouter">
+                    </form>
+
+                    <?php
+                        if (isset ($_POST ['nom']) && !empty($_POST ['nom']) && ($_POST ['marque']) && !empty($_POST ['marque']) && ($_POST ['date_constru']) && !empty($_POST ['date_constru']) && ($_POST ['origine']) && !empty($_POST ['origine']) && ($_POST ['moteur']) && !empty($_POST ['moteur']) && ($_POST ['prix']) && !empty($_POST ['prix']) && ($_POST ['image']) && !empty($_POST ['image'])){
+                            $ajout = new update();
+                            $ajout -> update($_POST ['nom'], $_POST ['marque'], $_POST ['date_constru'], $_POST ['origine'], $_POST ['moteur'], $_POST ['prix'], $_POST ['image']);
+                        } 
+                    ?>
                 
+
             </div>
             <div id="delete" align="center">
                 <div class="titre">
